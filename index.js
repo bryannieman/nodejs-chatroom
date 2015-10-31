ee5152784a410b0289ef324f78e80bed911352c0
Bryan Man <bryan@bryannie.com>
2015-10-31 12:35:08 +0800

    some optimization for code and data object

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use('/public', express.static('public'));
+app.use('/public', express.static('public'));  //mapping /public folder to the world as /public
 
-var port=740;
+var port=740;		//port used for server
 
-var remoteip;
-var shortip;
+var remoteip;		//remote IP in full
+var shortip;		//remote IP in short
+
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -17,25 +19,46 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  var dtCur;		//current date time
+  var blocks;		//keeping IP portions array
+  var s;
+  
   remoteip = socket.client.conn.remoteAddress;
-  var blocks = remoteip.split(":");
-  shortip = blocks[blocks.length-1];
-  //var blocks = remoteip.split(".");
-  //shortip = blocks[2] + "." + blocks[3] ;
+  //var blocks = remoteip.split(":");
+  //shortip = blocks[blocks.length-1];
+  blocks = remoteip.split(".");
+  shortip = blocks[2] + "." + blocks[3] ;
   //console.log(socket.request);
+  dtCur = new Date();
+  dtStr = dtCur.getHours() + ":" + dtCur.getMinutes();
   
-  io.emit('status message', '[' + shortip + '] connected');
-  console.log('[' + remoteip + '] connected');
+  // CONNECTED EVENT
+  s = [ dtStr,  " [", shortip, "] CONNECTED"].join("");
+  io.emit('status_msg', s);
+  s = [ dtStr,  " [", remoteip, "] CONNECTED"].join("");
+  console.log(s);
   
-  socket.on('chat message', function(msg){
-    io.emit('chat message', '[' + shortip + '] ' + msg);
-    console.log('message: [' + remoteip + '] ' + msg);
+  // DISCONNECTED EVENT
+  socket.on('disconnect', function(){
+    s = [ dtStr,  " [", shortip, "] DISCONNECTED"].join("");
+    io.emit('status_msg', s);
+    s = [ dtStr,  " [", remoteip, "] DISCONNECTED"].join("");
+    console.log(s);
+  });
+  
+  // SEND MESSAGE EVENT
+  socket.on('chat_msg', function(data){
+    var nickname = data.nickname;
+    var message  = data.message;
+    
+    s = [dtStr,  " [", shortip, "] ", nickname, " : ",  message].join("");
+    io.emit('chat_msg', s);
+    s = [dtStr,  " [", remoteip, "] ", nickname, " : ",  message].join("");
+    console.log(s);
+    
   });
 
-  socket.on('disconnect', function(){
-    io.emit('status message', '[' + shortip + '] disconnected');
-    console.log(remoteip +' disconnected');
-  });
+
 });
 
 http.listen(port, function(){
